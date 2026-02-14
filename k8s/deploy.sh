da96626c113e34ab01ac24c558da2ff2c106afdf
Marcin Wierzbiński <marcin119a@gmail.com>
2026-02-14 15:52:29 +0100

    ingress, pvc, configmap, resources, hpa

--- k8s/deploy.sh
+++ k8s/deploy.sh
@@ -3,3 +3,9 @@ kubectl apply -f worker-deployment.yml
 kubectl apply -f redis-pod.yml
 kubectl apply -f api-service.yml
 kubectl apply -f redis-service.yml
+
+# nowe confi 
+kubectl apply -f api-config.yml
+kubectl apply -f api-hpa.yml
+kubectl apply -f worker-pvc.yaml
+kubectl apply -f ingress.yml
